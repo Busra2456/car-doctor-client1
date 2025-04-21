73d9a80c206269b3472e1de036c089d637295ece
Busra2456 <you@example.com>
2025-04-21 16:41:27 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import About from "../About/About";
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+      return (
+            <div>
+                  <h1>this is a Home</h1>
+                  <Banner></Banner>
+                  <About></About>
+            </div>
+      );
+};
+
+export default Home;
